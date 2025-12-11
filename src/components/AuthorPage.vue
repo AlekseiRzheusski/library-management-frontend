28c6a0da943ca70dcf865f4d829fb06f473f493d
a.rzheussky <a.rzheussky@syberry.com>
2025-12-11 19:11:01 +0300

    feat: add Author Page

---
+++ src/components/AuthorPage.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { getAuthor } from "../services/authorService";
+
+const route = useRoute();
+
+const author = ref(null);
+const error = ref(null);
+
+onMounted(async () => {
+  try {
+    const idParam = route.params.id;
+    const idAsString = Array.isArray(idParam) ? idParam[0] : idParam;
+    const authorId = BigInt(idAsString);
+
+    author.value = await getAuthor(authorId);
+
+    console.debug(author.value.title);
+    error.value = null;
+  } 
+  catch (err) {
+    console.error(err);
+    error.value = err.message;
+    author.value = null;
+  }
+});
+</script>
+
+<template>
+  <div v-if="error" style="color:red">{{ error }}</div>
+
+  <div v-else-if="author">
+    <p>ID: {{ author.authorId.toString() }}</p>
+    <p>Name: {{ author.firstName }} {{ author.LastName }}</p>
+    <p>Biography: {{ author.biography }}</p>
+    <p>Date of birth: {{ author.dateOfBirth }}</p>
+    <p>Book Count: {{ author.bookCount }}</p>
+    <p>{{author.isAvailable ? 'Is active' : 'Is not active' }}</p>
+  </div>
+</template>
