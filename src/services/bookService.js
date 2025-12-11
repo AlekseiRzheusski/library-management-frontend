c38eb33c2203840e8f4c62692fa040d37577aae4
a.rzheussky <a.rzheussky@syberry.com>
2025-12-11 15:25:52 +0300

    feat: add BookSearchPage

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -4,3 +4,8 @@ export async function getBook(id) {
   const response = await api.get(`/book/${id}`)
   return response.data
 }
+
+export async function getBooksPage(params) {
+  const response = await api.get('/book/list', { params });
+  return response.data
+}
